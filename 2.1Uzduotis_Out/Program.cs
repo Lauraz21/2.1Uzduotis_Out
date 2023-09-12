92a140a070c5213c11d42bb538ea29417c92c536
Laura <Laura@DESKTOP-BESSB6E>
2023-09-12 20:04:55 +0300

    Add project files.

---
+++ 2.1Uzduotis_Out/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2._1Uzduotis_Out
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Iveskite skaiciu: ");
+                bool convert = int.TryParse(Console.ReadLine(), out int userInput);
+
+                if (userInput > 100 || !convert)
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+}
